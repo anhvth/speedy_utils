890e360ad76a2b708493b80fd281edfe03f99983
Anh. Võ Trần Hải (5) <anhvth5@vng.com.vn>
2024-09-29 16:19:44 +0700

    bump

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
-rm -rf dist/ build/ *.egg-info
-pip install -r requirements.txt
-poetry build
-twine upload dist/* 
+# rm -rf dist/ build/ *.egg-info
+# pip install -r requirements.txt
+# poetry build
+# twine upload dist/* 
+bump2version patch
+git add -A && git commit -m "Bumped version"
+git push 
