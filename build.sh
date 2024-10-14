7206f37fbac531d840c70fe5042a0b03ed72c959
anh vo <anhvth.226@gmail.com>
2024-10-14 18:13:54 +0700

    udpate

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
-# rm -rf dist/ build/ *.egg-info
-# pip install -r requirements.txt
-# poetry build
-# twine upload dist/* 
 bump2version patch
 git add -A && git commit -m "Bumped version"
 git push 
