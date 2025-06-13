25b6e40018fd9e9d960f86804a9695b6da45719b
anh vo <anhvth.226@gmail.com>
2025-06-13 12:58:53 +0700

    update

--- src/speedy_utils/common/utils_print.py
+++ src/speedy_utils/common/utils_print.py
@@ -1,32 +1,13 @@
 # utils/utils_print.py
 
 import copy
-import inspect
-import json
 import pprint
-import re
-import sys
 import textwrap
-import time
-from collections import OrderedDict
-from typing import Annotated, Any, Dict, List, Literal, Optional
+from typing import Any
 
-from IPython.display import HTML, display
-from loguru import logger
 from tabulate import tabulate
 
-from .utils_misc import is_notebook
-
-
-def display_pretty_table_html(data: dict) -> None:
-    """
-    Display a pretty HTML table in Jupyter notebooks.
-    """
-    table = "<table>"
-    for key, value in data.items():
-        table += f"<tr><td>{key}</td><td>{value}</td></tr>"
-    table += "</table>"
-    display(HTML(table))
+from .notebook_utils import display_pretty_table_html
 
 
 # Flattening the dictionary using "." notation for keys
@@ -166,51 +147,7 @@ def fprint(
         printer.pprint(processed_data)
 
 
-def print_table(data: Any, use_html: bool = True) -> None:
-    """
-    Print data as a table. If use_html is True, display using IPython HTML.
-    """
-
-    def __get_table(data: Any) -> str:
-        if isinstance(data, str):
-            try:
-                data = json.loads(data)
-            except json.JSONDecodeError as exc:
-                raise ValueError("String input could not be decoded as JSON") from exc
-
-        if isinstance(data, list):
-            if all(isinstance(item, dict) for item in data):
-                headers = list(data[0].keys())
-                rows = [list(item.values()) for item in data]
-                return tabulate(
-                    rows, headers=headers, tablefmt="html" if use_html else "grid"
-                )
-            else:
-                raise ValueError("List must contain dictionaries")
-
-        if isinstance(data, dict):
-            headers = ["Key", "Value"]
-            rows = list(data.items())
-            return tabulate(
-                rows, headers=headers, tablefmt="html" if use_html else "grid"
-            )
-
-        raise TypeError(
-            "Input data must be a list of dictionaries, a dictionary, or a JSON string"
-        )
-
-    table = __get_table(data)
-    if use_html:
-        display(HTML(table))
-    else:
-        print(table)
-
-
 __all__ = [
-    "display_pretty_table_html",
     "flatten_dict",
     "fprint",
-    "print_table",
-    # "setup_logger",
-    # "log",
 ]
