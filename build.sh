52f9053425af74bc6733b936df9b0979fdf23d89
anh vo <anhvth.226@gmail.com>
2025-06-19 15:12:20 +0700

    Bumped version to 1.0.21

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@ if ! bump2version --allow-dirty patch; then
     echo "Error: bump2version failed"
     exit 1
 fi
-
-git add -A && git commit -m "Bumped version"
+version=$(bump2version --allow-dirty --dry-run --list patch | grep new_version | cut -d '=' -f2 | xargs)
+message="Bumped version to $version"
+git add -A && git commit -m "$message"
 git push
