f8d93a2a93df06495cd5dcb4f0f93aa1d7b02b63
anh vo <anhvth.226@gmail.com>
2025-07-10 20:57:53 +0700

    update

--- bumpversion.sh
+++ bumpversion.sh
@@ -5,8 +5,15 @@ set -e
 echo 'Current version:'
 poetry version
 
-echo 'Bumping patch version...'
-poetry version patch
+# Determine bump type (patch, minor, major)
+BUMP_TYPE=${1:-patch}
+if [[ "$BUMP_TYPE" != "patch" && "$BUMP_TYPE" != "minor" && "$BUMP_TYPE" != "major" ]]; then
+	echo "Usage: $0 [patch|minor|major]"
+	exit 1
+fi
+
+echo "Bumping $BUMP_TYPE version..."
+poetry version $BUMP_TYPE
 
 NEW_VERSION=$(poetry version -s)
 echo "New version: $NEW_VERSION"
